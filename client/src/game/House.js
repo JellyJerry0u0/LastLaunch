9d3d818a87b450660b8ba8fc3975b565f44cc13f
jaemina-a <tkfkd@unist.ac.kr>
2025-07-20 02:56:00 +0900

    house

---
+++ client/src/game/House.js
@@ -0,0 +1,16 @@
+export default class House {
+  constructor(scene, x, y, size = 120, color = 0xffa500) {
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.createHouse();
+  }
+
+  createHouse() {
+    this.graphics = this.scene.add.graphics();
+    this.graphics.fillStyle(this.color, 1);
+    this.graphics.fillRect(this.x, this.y, this.size, this.size);
+  }
+} 
---
+++ client/src/game/Map.js
@@ -0,0 +1,23 @@
+// client/src/game/Map.js
+
+const GAME_WIDTH = 2000;
+const GAME_HEIGHT = 3000;
+const TILE_SIZE = 80;
+
+export default class GameMap {
+  constructor(scene) {
+    this.scene = scene;
+    this.createBase();
+  }
+
+  createBase() {
+    const g = this.scene.add.graphics();
+    for (let y = 0; y < GAME_HEIGHT; y += TILE_SIZE) {
+      for (let x = 0; x < GAME_WIDTH; x += TILE_SIZE) {
+        const isEven = ((x / TILE_SIZE) + (y / TILE_SIZE)) % 2 === 0;
+        g.fillStyle(isEven ? 0x333333 : 0x0, 1);
+        g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+      }
+    }
+  }
+} 
