6fc6e52b63c10d0927fa491d59a3eacaed815b4b
krpark <qkr789rudfl@sookmyung.ac.kr>
2025-07-21 20:12:13 +0000

    ya

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ npm run build
 
 cd /home/ubuntu/LastLaunch/server
 npm install
-pm2 restart Last-Launch-server || pm2 start src/index.js --name Last-Launch-server
+pm2 restart last-launch-server --update-env || pm2 start src/index.js --name last-launch-server --update-env
 
