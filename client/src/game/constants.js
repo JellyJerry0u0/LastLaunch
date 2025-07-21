ebcc5c50508e6cf1bdb99244177122b1516d6c76
jaemina-a <tkfkd@unist.ac.kr>
2025-07-21 13:37:42 +0900

    scene

--- client/src/game/constants.js
+++ client/src/game/constants.js
@@ -2,6 +2,20 @@ export const GAME_WIDTH = 2500;
 export const GAME_HEIGHT = 2500;
 export const TILE_SIZE = 80;
 
+export const MAIN_MAP_WIDTH = 2500;
+export const MAIN_MAP_HEIGHT = 2500;
+
+export const INITIAL_POSITION = {
+    "StartingPoint": {
+        "x": 100,
+        "y": 100
+    },
+    "Farm": {
+        "x": MAIN_MAP_WIDTH / 2,
+        "y": MAIN_MAP_HEIGHT / 2
+    }
+}
+
 // 오브젝트 크기
 export const HOUSE_SIZE = 120;
 export const MINE_SIZE = 200;
@@ -9,3 +23,4 @@ export const MINE_SIZE = 200;
 // 색상 등 기타 상수
 export const HOUSE_COLOR = 0xffa500;
 export const MINE_COLOR = 0x888888;
+
